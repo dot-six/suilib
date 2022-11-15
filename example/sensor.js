e10be52259dbe9c2c75d1327511bd0491bf66e56
Hanz <hanz@godot.id>
2022-11-15 13:39:50 +0700

    chore: Deleted example/

--- example/sensor.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const { Sensor } = require('../lib/sfml');
-console.log(Object.fromEntries(Object.entries(Sensor.Type).map(
-  ([ k, v ]) => ([ k, Sensor.isAvailable(v) ]))));
-
-Object.values(Sensor.Type).forEach(type => Sensor.setEnabled(type, true));
-console.log(Object.fromEntries(Object.entries(Sensor.Type).map(
-  ([ k, v ]) => ([ k, Sensor.getValue(v) ]))));
--- example/touch.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const { Touch } = require('../lib/sfml');
-
-console.log(Touch.isDown(0));
-console.log(Touch.getPosition(0));
