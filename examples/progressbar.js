619ac8eaa20081c024aa03d6756abbac5c9309f0
Hanz <40712686+HanzCEO@users.noreply.github.com>
2023-02-04 16:57:04 +0700

    docs(examples/progressbar): Integrated interval system

--- examples/progressbar.js
+++ examples/progressbar.js
@@ -1,15 +1,17 @@
 let { Window, ProgressBar } = require('../');
 
 let w = new Window(500, 500);
-let bar = new ProgressBar(0, 1000);
+let bar = new ProgressBar(0, 100);
 
-bar.size.x = 200;
-bar.size.y = 20;
+bar.size.x = 100;
+bar.size.y = 10;
 
 w.addChild(bar);
-w.mainLoopWhile(() => {
-    if (bar.value < 1000) {
+
+w.setInterval(() => {
+    if (bar.value < 100) {
         bar.value += 1;
     }
-    return true;
-});
+}, 100);
+
+w.mainLoopWhile();
